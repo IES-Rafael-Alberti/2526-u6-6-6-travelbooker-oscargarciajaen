869782f9e17f37c984eb427b74bc16858cfee513
oscargarcia <ogarjae565@g.educaand.es>
2026-03-11 16:23:26 +0100

    Clase consola creada y método mostrarMenu implementado

---
+++ src/main/kotlin/presentacion/Consola.kt
@@ -0,0 +1,22 @@
+package org.practicatrim2.presentacion
+
+class Consola {
+
+    fun mostrarMenu(): String{
+        println("Elige una Opción 1 o 2")
+        println("1. Crear Reserva")
+        println("2. Mostrar Reserva")
+
+
+        var seleccion: String? = null
+
+        do {
+            print("\nTú opción -> ")
+            val entrada = readLine()
+            if (entrada != null && (entrada == "1" || entrada == "2") ) {
+                seleccion = entrada
+            }
+        } while(seleccion == null)
+        return seleccion
+    }
+}
