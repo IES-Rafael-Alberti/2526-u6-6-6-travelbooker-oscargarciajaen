ab58fd689c4457dd6979d66221d45ecbb9e73966
oscargarcia <ogarjae565@g.educaand.es>
2026-03-11 16:39:01 +0100

    Método solicitarTipoReserva implementado en la clase Consola

--- src/main/kotlin/presentacion/Consola.kt
+++ src/main/kotlin/presentacion/Consola.kt
@@ -7,7 +7,6 @@ class Consola {
         println("1. Crear Reserva")
         println("2. Mostrar Reserva")
 
-
         var seleccion: String? = null
 
         do {
@@ -18,5 +17,22 @@ class Consola {
             }
         } while(seleccion == null)
         return seleccion
+    }
+
+    fun solicitarTipoReserva(): String{
+        println("\nSelecciona el tipo de reserva 1 o 2")
+        println("1. Reserva de Hotel")
+        println("2. Reserva de Vuelo")
+
+        var reserva: String? = null
+
+        do {
+            print("\nTú opción -> ")
+            val entrada = readLine()
+            if (entrada != null && (entrada == "1" || entrada == "2") ) {
+                reserva = entrada
+            }
+        } while(reserva == null)
+        return reserva
     }
 }
