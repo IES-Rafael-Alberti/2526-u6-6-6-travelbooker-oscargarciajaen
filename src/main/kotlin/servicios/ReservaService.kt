672198ab6b582df9ee711e0e979faffc4f559dcc
oscargarcia <ogarjae565@g.educaand.es>
2026-03-12 17:03:37 +0100

    capa de servicios completada (en principio)

--- src/main/kotlin/servicios/ReservaService.kt
+++ src/main/kotlin/servicios/ReservaService.kt
@@ -1,21 +1,46 @@
 package org.practicatrim2.servicios
 
+import org.practicatrim2.Dominio.Reserva
 import org.practicatrim2.Dominio.ReservaHotel
+import org.practicatrim2.Dominio.ReservaVuelo
+import org.practicatrim2.datos.Repositorio
 import org.practicatrim2.presentacion.Consola
 
-class ReservaService(): IReservaRepository {
+class ReservaService (): IReservaRepository {
+
+    val reservasHoteles : MutableMap<Int, Reserva> = Repositorio().reservasHotel
+
+    val reservasVuelos : MutableMap<Int, Reserva> = Repositorio().reservasVuelo
 
     override fun agregar(seleccion: String) {
+        var reserva: Reserva? = null
         when (seleccion){
-            "1" -> ReservaHotel.crearInstancia(
+            "1" -> reserva = ReservaHotel.crearInstancia(
                 Consola.solicitarUbicacion(),
                 Consola.solicitarNumeroNoches(),
-                pene
-                )
+                Consola.solicitarFecha(),
+                Consola.solicitarDescripción())
+
+            "2" -> reserva = ReservaVuelo.crearInstancia(
+                Consola.solicitarOrigen(),
+                Consola.solicitarDestino(),
+                Consola.solicitarHoraVuelo(),
+                Consola.solicitarFecha(),
+                Consola.solicitarDescripción()
+            )
+        }
+        when (reserva){
+            is ReservaHotel -> reservasHoteles[reserva.id] = reserva
+            is ReservaVuelo -> reservasVuelos[reserva.id] = reserva
         }
     }
 
     override fun obtenerTodas() {
-
+        if (reservasHoteles.isNotEmpty()){
+            reservasHoteles.forEach { Consola.mostrarReserva(it.key, it.value)  }
+        }
+        if (reservasVuelos.isNotEmpty()){
+            reservasVuelos.forEach { Consola.mostrarReserva(it.key, it.value) }
+        }
     }
 }
