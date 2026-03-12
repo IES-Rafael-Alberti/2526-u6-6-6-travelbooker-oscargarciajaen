23c4781477daf7c91d68212f0f3422749baeb4f2
oscargarcia <ogarjae565@g.educaand.es>
2026-03-12 12:30:39 +0100

    Implementación de funciones de la clase consola

--- src/main/kotlin/presentacion/Consola.kt
+++ src/main/kotlin/presentacion/Consola.kt
@@ -1,5 +1,7 @@
 package org.practicatrim2.presentacion
 
+import java.time.LocalDate
+
 class Consola {
 
     fun mostrarMenu(): String{
@@ -34,5 +36,59 @@ class Consola {
             }
         } while(reserva == null)
         return reserva
+    }
+
+    companion object{
+
+        fun solicitarUbicacion(): String{
+            println("Introduzca la ubicación")
+            var ubicacion: String? = null
+
+            do {
+                print("\nTú respuesta -> ")
+                val entrada = readLine()
+                if (entrada != null && entrada.isNotEmpty()) {
+                    ubicacion = entrada
+                }
+            } while(ubicacion == null)
+            return ubicacion
+        }
+
+        fun solicitarNumeroNoches(): Int{
+            println("Introduzca la ubicación")
+            var numeroNoches: Int? = null
+
+            do {
+                print("\nTú respuesta -> ")
+                val entrada = readLine()
+                if (entrada != null && entrada.all { it.isDigit() }) {
+                    val entradaEntera = entrada.toInt()
+                    if (entradaEntera > 0 && entradaEntera < 100){
+                        numeroNoches = entradaEntera
+                    }
+                }
+            } while(numeroNoches == null)
+            return numeroNoches
+        }
+
+        fun solicitarFecha(): LocalDate{
+            println("Introduce la fecha en formato YYYY-MM-DD")
+
+            var fecha: String? = null
+            do {
+                print("\nTú respuesta -> ")
+                val entrada = readLine()
+                val fecharRegex = "[2][0][2][6]-[0-1][0-9]-[0-3][0-9]".toRegex()
+                if (entrada != null  && entrada.matches(fecharRegex)) {
+                    fecha = entrada
+                }
+            } while(fecha == null)
+
+            val fechaFormateada = LocalDate.parse(fecha)
+
+            return fechaFormateada
+        }
+
+        fun solicitarDescripción
     }
 }
