490bf49262997e5442e89713ee71c58c783bbdfc
oscargarcia <ogarjae565@g.educaand.es>
2026-03-13 08:33:07 +0100

    Documentada clase reserva

--- src/main/kotlin/datos/Repositorio.kt
+++ src/main/kotlin/datos/Repositorio.kt
@@ -2,8 +2,19 @@ package org.practicatrim2.datos
 
 import org.practicatrim2.Dominio.Reserva
 
+
+/**
+ * Repositorio que contiene las reservas
+ *
+ */
 class Repositorio() {
+    /**
+     * Mapa mutable para introducir las reservas de hoteles, usando el id como clave y el objeto como valor.
+     */
     val reservasHotel: MutableMap<Int, Reserva> = mutableMapOf()
 
+    /**
+     * Mapa mutable para introducir las reservas de vuelos, usando el id como clave y el objeto como valor.
+     */
     val reservasVuelo: MutableMap<Int, Reserva> = mutableMapOf()
 }
